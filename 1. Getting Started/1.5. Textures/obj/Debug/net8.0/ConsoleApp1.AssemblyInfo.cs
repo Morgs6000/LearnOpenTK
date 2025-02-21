940829e9b5627105e94178570140c4405f236903
Morgana Stradivarius <55167884+Morgs6000@users.noreply.github.com>
2025-02-21 07:19:18 -0300

    Commit Update

--- 1. Getting Started/1.5. Textures/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ 1. Getting Started/1.5. Textures/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62bd057d12b2fd0f79e5c668b4724b2b1d42c775")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+085ae4dc63aebde1f3c5edfc26589dd774844fd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 2. Lighting/2.2.2. Basic Lighting - Specular/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ 2. Lighting/2.2.2. Basic Lighting - Specular/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26a2b14a2470a2491180503061e438e0f266b457")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+085ae4dc63aebde1f3c5edfc26589dd774844fd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
